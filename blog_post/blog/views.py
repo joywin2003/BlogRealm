979723116a65ab8064cf547a59f5dc885c9df11f
joywin2003 <21a20.joywin@sjec.ac.in>
2023-06-14 20:23:58 +0530

    added template inheritence

--- blog_post/blog/views.py
+++ blog_post/blog/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-# Create your views here.
+
+
+
+posts = [
+    {
+        'author': 'Sam',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2020',
+    },
+    {
+        'author': 'Joy',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2020',
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog page</h1>')
+    context = {'posts': posts}
+    return render(request, 'blog/blog.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html',{'title': 'About'})
+
+
