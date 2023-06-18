858137ac57ff99c1b04b6e86a41d1d13c206f8c6
joywin2003 <21a20.joywin@sjec.ac.in>
2023-06-18 16:46:55 +0530

    added profile pic

--- blog_post/blog_post/settings.py
+++ blog_post/blog_post/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +133,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = 'blog-home' 
 
 LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_URL = '/media/'
