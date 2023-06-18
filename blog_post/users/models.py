6ea871f8d47c060ef722732e7bd068dee4d3bd59
joywin2003 <21a20.joywin@sjec.ac.in>
2023-06-18 18:22:28 +0530

    added profile update and blog profile

--- blog_post/users/models.py
+++ blog_post/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,4 +8,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+    
+    def save(self):
+        super().save()
+
+        if self.image.height >300 or self.image.width >300:
+            output_size = (300,300)
+            img = Image.open(self.image.path)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 
