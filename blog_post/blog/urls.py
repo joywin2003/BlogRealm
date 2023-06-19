61d59c08354e735d66a82c38d9fda056da2276b0
joywin2003 <21a20.joywin@sjec.ac.in>
2023-06-19 22:17:40 +0530

    Listviews and detailviews

--- blog_post/blog/urls.py
+++ blog_post/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import PostListView, PostDetialView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('',PostListView.as_view() , name='blog-home'),
+    path('post/<int:pk>/',PostDetialView.as_view() , name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog_post/blog/views.py
+++ blog_post/blog/views.py
@@ -1,11 +1,22 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from .models import Post
+
 
 
 
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blog/blog.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/blog.html'
+    context_object_name = 'posts'
+    ordering = '-date_posted'
+
+class PostDetialView(DetailView):
+    model = Post
 
 
 def about(request):
